f0048945d778579d8cc99b707fdf0ba4c8c513da
zvi741 <51421153+zvi741@users.noreply.github.com>
2020-05-05 13:28:59 +0300

    minor changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 // Allow Cross-Origin Requests
 app.use(cors());
 
-mongoose.connect("mongodb+srv://test:test@cluster0-s2npo.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://test:<password>@cluster0-s2npo.mongodb.net/test?retryWrites=true&w=majority");
 mongoose.connection.once('open', () => {
   console.log('Connected to db');
 });
